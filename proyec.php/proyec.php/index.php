290a762051febd6b8954b9b4fb2b507ec6df1559
ediuwu <86217157+ediuwu@users.noreply.github.com>
2021-06-22 19:14:14 -0500

    Add files via upload

---
+++ proyec.php/proyec.php/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+    <body style="color:red;size:100px">
+    				<body background="Eve.jpeg"></body>
+<center><font face="Comic Sans MS,TIMES NEW ROMAN,verdana">ESE2021
+<br>
+<br>
+FORMULARIO DE REGISTRO</font>
+<br></Center>
+
+<html lang="en">
+				<head>
+					<meta charset="UTF -8">
+					<link rel="stylesheet" href="style.css"
+					</head>
+					<body>
+									<section class="from-register">
+											
+					<center> <input class="controls" type="text" name="nombres" id="Nombres" placeholder="Ingrese nombre">
+						<input class="controls" type="email" name="correo" id="correo" placeholder="Ingrese correo">
+						<input class="controls" type="password" name="contraseña" id="contraseña" placeholder="Ingrese contraseña">
+						<p>Estoy de acuerdo de acuerdo <a href="#">Terminos y Condiciones</a></p>
+						<input class="botons" type="submit" value="Registrar">
+						<p><a href="#">¿ya tengo cuenta?</a></p>
+
+						
+									</section>
+					</body>
+				</center>
+<html>
+<head>
+	<meta charset="UTF -8">
+					<meta htt-equiv="X-UA-Compatible" content="ie-=edge">
+					<link rel="stylesheet" href="stylefir.css">
+						<meta charset="UTF-8">
+	<title>ese2021</title>
+	<link rel="stylesheet" href="nn.css">
+</head>
+<body>
+
+		
+<center><font face="Comic Sans MS,TIMES NEW ROMAN,verdana">ESE2021
+<br>
+<br>
+INICIO</font>
+<br></Center>
+        <br>
+									
+									<section class="from-registert">
+												
+					<center><input class="controls" type="text" name="usuario" id="Usuario" placeholder="Ingrese usuario">
+						<input class="controls" type="password" name="contraseña" id="contraseña" placeholder="Ingrese contraseña">
+						<br><br>
+						
+						<input class="botons" type="submit" value="Entrar">
+						<input class="botons" type="submit" value="Registrar">
+						<br>
+						<br>
+						<p><a href="#"> ¿Olvidaste tu contraseña?</a></p>
+
+									</section>					
+
+									<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ese2021</title>
+	<link rel="stylesheet" href="nn.css">
+	<input class="botons" type="submit" value="REGRESAR">
+</head>
+<body>
+	<header>
+		
+
+									<header>
+		<nav class="navegacion">
+			<ul class="menu">
+				 <li><a href="#">ARTISTAS</a>
+				 <ul class="submenu">
+				 		<li><a href="#">Carla Morrrison</a></li>
+				 		<li><a href="#">Lil Peep</a></li>
+				 		<li><a href="#">Rels b</a></li>
+				 		
+				 </ul>
+				 </li>
+				  <li><a href="#">NOVEDADES</a>
+				  	<ul class="submenu">
+				 		<li><a href="#">Location-J Balvin</a></li>
+				 		<li><a href="#">Ropa cara-Camilo</a></li>
+				 		<li><a href="#">Estas que te pelas-Carla Morrison</a></li>
+
+				 </ul>
+				   </li>
+
+
+			</ul>
+		</nav>
+
+	</header>
+
+<div class="contenedor">
+								<header>
+												<h1>ARTISTAS</h1>
+								</header>	
+								<section class="main">
+												<html>
+												<body>
+												<p>
+						 ARTISTAS MAS ESCUCHADOS A NIVEL GLOBAL <br>
+
+                -Bad Bunny<br>
+                <img src="descarga.jpg" style="width: 250px; height: 150 px;"><br>
+                -Drake<br>
+                <img src="descarga (1).jpg  " style="width: 250px; height: 150px;"><br>
+                -J Balvin<br>
+                <img src="descarga (2).jpg" style="width: 250px; height: 140px;"><br>
+                -Juice WRLD<br>
+                <img src="descarga (3).jpg" style="width: 250px; height: 150px;"><br>
+                -The Weeknd<br><br>
+                <img src="descarga (4).jpg" style="width: 250px; height: 160px;"><br>
+							Artistas Más Escuchadas a nivel global<br>
+        -Billie Eilish.<br>
+        <img src="descarga (5).jpg" style="width: 250px; height: 170px;"><br>
+        -Taylor Swift.<br>
+        <img src="descarga (69.jpg" style="width: 270px; height: 150px;"><br>
+        -Ariana Grande.<br>
+        <img src="descarga (7).jpg" style="width: 250px; height: 150px;"><br>
+        -Carla Morrisoon<br>
+        <img src="descarga (8).jpg" style="width: 250px; height: 160px;"><br><br><br><br>
+        
+												
+												</p>
+												
+								</section>
+								<aside>
+
+<div class="Fb">
+												<p> 
+																<body style="color:blank;size:100px">
+<font face="Comic Sans MS,COURIER,verdana"> Artistas con más streams a nivel mundial</font>
+<br>
+</body>
+															
+	Este año, el puertorriqueño Bad Bunny se adjudica el primer lugar este año con más de 8.300 millones de streams de fanáticos de todo el mundo. Su álbum YHLQMDLG, lanzado a fines de febrero y con colaboraciones con Sech, Anuel AA y Daddy Yankee, es también el álbum número uno en streams. Siguiendo a Bad Bunny están Drake y la estrella de Colombia J Balvin. El cuarto artista más reproducido es el fallecido rapero Juice WRLD, seguido por The Weeknd.<br><br>
+
+Billie Eilish continúa su reinado como la artista femenina más escuchada en Spotify por segundo año consecutivo, seguida por Taylor Swift y Ariana Grande en segundo y tercer lugar. Dua Lipa y Halsey se ubican como la cuarta y quinta artistas femeninas con más streams.
+
+
+																
+												</p>
+								</aside>
+								
+										<div class="video">
+<br><br><br><br>
+<h1>Carla Morrison-Estas que te pelas</h1>
+
+<video controls>
+<source src="video-1622260372.mp4">
+
+</video>
+</div>
+								<footer>
+												<p>
+				Canciones más escuchadas a nivel mundial<br>
+La canción más reproducida del año es “Blinding Lights” de The Weeknd, que se mantuvo firme después de ser la segunda canción más reproducida del verano con casi 1.600 millones de reproducciones este año. En el segundo y tercer lugar, respectivamente, están “Dance Monkey“, del ex músico callejero australiano Tones And I, que se volvió viral en 2019, y “The Box.” de Roddy Ricch. La cuarta canción más reproducida es la pegajosa “Roses – Imanbek Remix” de Imanbek y SAINt JHN, seguida de “Don’t Start Now” de Dua Lipa.
+												</p>
+							
+<br>
+</body>
+								</footer>
+				</div>
+		
+</body>
+</html>
